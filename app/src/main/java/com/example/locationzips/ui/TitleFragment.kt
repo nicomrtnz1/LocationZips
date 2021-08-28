4b68e745c3b8ae4ac8530ea793d106be24f8cb4f
Nico Martinez <nico.martinez@jbhunt.com>
2021-08-28 10:30:23 -0500

    Changes to title fragment logo

--- app/src/main/java/com/example/locationzips/ui/TitleFragment.kt
+++ app/src/main/java/com/example/locationzips/ui/TitleFragment.kt
@@ -5,30 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.navigation.Navigation
 import com.example.locationzips.R
 import com.example.locationzips.databinding.FragmentTitleBinding
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 
 
 class TitleFragment : Fragment() {
 
     private var _binding: FragmentTitleBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentTitleBinding.inflate(inflater, container, false)
         val startButton = binding.startButton
         startButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.searchFragment)
         )
+        val largeCircle = binding.lgCircle
+        val smallCircle = binding.smCircle
+
+        startAnimation(largeCircle,1.8f)
+        startAnimation(smallCircle,8f)
         return binding.root
+    }
+
+    private fun startAnimation(target: View, scale:Float){
+        val scaleDown: ObjectAnimator = ObjectAnimator.ofPropertyValuesHolder(
+            target,
+            PropertyValuesHolder.ofFloat("scaleX", scale),
+            PropertyValuesHolder.ofFloat("scaleY", scale)
+        )
+        scaleDown.duration = 1500
+
+        scaleDown.repeatCount = ObjectAnimator.INFINITE
+        scaleDown.repeatMode = ObjectAnimator.REVERSE
+
+        scaleDown.start()
     }
 
     override fun onDestroyView() {
